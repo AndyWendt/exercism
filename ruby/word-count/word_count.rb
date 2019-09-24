70cffe15ee705f25644caf80f1509b10a538b3bd
Andy Wendt <andy@awendt.com>
2019-09-24 09:27:02 -0600

    Add more passing tests

--- ruby/word-count/word_count.rb
+++ ruby/word-count/word_count.rb
@@ -1,10 +1,11 @@
 class Phrase
   def initialize(string)
-    @words = string.gsub(/[^0-9A-Za-z[:punct:]]/, '').split(/(?:[[:punct:], \s]+)/)
+    @words = string.gsub(/[^0-9A-Za-z[:punct:]] /, '').split(/(?:[[:punct:], \s]+)/)
   end
 
   def word_count
     @words.reduce({}) do |hash, word|
+      word = word.downcase
       current_count = hash[word] ? hash[word] + 1 : 1
       hash[word] = current_count
       hash
