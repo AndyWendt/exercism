43b411dc3998350d28ef732d197de9651fc5bece
Andy Wendt <andy@awendt.com>
2019-09-23 18:16:05 -0600

    Acronym lowercase passing

--- ruby/acronym/acronym.rb
+++ ruby/acronym/acronym.rb
@@ -2,6 +2,6 @@ class Acronym
   def self.abbreviate(string)
     words = string.split(' ')
     puts words
-    words.reduce('') { |acronym, word| acronym + word[0] }
+    words.reduce('') { |acronym, word| acronym + word[0].upcase }
   end
 end
