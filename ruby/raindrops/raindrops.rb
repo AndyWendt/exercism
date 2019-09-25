fc5d6fd2ba9c8f4a2f92c3386c9fdabe0c4863e0
Andy Wendt <andy@awendt.com>
2019-09-24 22:34:10 -0600

    Refactor and passing all tests

--- ruby/raindrops/raindrops.rb
+++ ruby/raindrops/raindrops.rb
@@ -2,13 +2,20 @@ require 'prime'
 
 class Raindrops
   def self.convert(number)
-    factors = number.prime_division.flatten
-    raindrops = []
-    raindrops.push('Pling') if factors.include?(3)
-    raindrops.push('Plang') if factors.include?(5)
-    raindrops.push('Plong') if factors.include?(7)
-    return raindrops.join('') unless raindrops.empty?
+    factors = number.prime_division.map { |factors| factors[0] }
+    sounds = listen_for_sounds(factors)
+    return sounds.join('') unless sounds.empty?
 
     number.to_s
+  end
+
+  private
+
+  def self.listen_for_sounds(factors)
+    sounds = []
+    sounds.push('Pling') if factors.include?(3)
+    sounds.push('Plang') if factors.include?(5)
+    sounds.push('Plong') if factors.include?(7)
+    sounds
   end
 end
--- ruby/raindrops/raindrops_test.rb
+++ ruby/raindrops/raindrops_test.rb
@@ -24,7 +24,6 @@ class RaindropsTest < Minitest::Test
   end
 
   def test_2_to_the_power_3_does_not_make_a_raindrop_sound_as_3_is_the_exponent_not_the_base
-    skip
     assert_equal "8", Raindrops.convert(8)
   end
 
