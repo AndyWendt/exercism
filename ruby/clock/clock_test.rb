05ea89a97d330a8242ba2462196215d727c6c8c8
Andy Wendt <andy@awendt.com>
2019-09-26 08:54:23 -0600

    Add passing tests

--- ruby/clock/clock_test.rb
+++ ruby/clock/clock_test.rb
@@ -100,31 +100,26 @@ class ClockTest < Minitest::Test
   end
 
   def test_add_more_than_one_hour
-    skip
     clock1 = Clock.new(hour: 10, minute: 0)
     assert_equal "11:01", (clock1 + Clock.new(minute: 61)).to_s
   end
 
   def test_add_more_than_two_hours_with_carry
-    skip
     clock1 = Clock.new(hour: 0, minute: 45)
     assert_equal "03:25", (clock1 + Clock.new(minute: 160)).to_s
   end
 
   def test_add_across_midnight
-    skip
     clock1 = Clock.new(hour: 23, minute: 59)
     assert_equal "00:01", (clock1 + Clock.new(minute: 2)).to_s
   end
 
   def test_add_more_than_one_day
-    skip
     clock1 = Clock.new(hour: 5, minute: 32)
     assert_equal "06:32", (clock1 + Clock.new(minute: 1500)).to_s
   end
 
   def test_add_more_than_two_days
-    skip
     clock1 = Clock.new(hour: 1, minute: 1)
     assert_equal "11:21", (clock1 + Clock.new(minute: 3500)).to_s
   end
