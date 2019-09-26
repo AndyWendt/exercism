d2ada9d7dd7873bbae84f7d6a236f92f6ff01113
Andy Wendt <andy@awendt.com>
2019-09-26 08:13:32 -0600

    Refactor to calculation

--- ruby/clock/clock.rb
+++ ruby/clock/clock.rb
@@ -1,10 +1,20 @@
 class Clock
-  def initialize(hour: nil, minute: nil)
+  def initialize(hour: 0, minute: 0)
     @hour = hour
     @minute = minute
   end
 
   def to_s
-    "08:00"
+    hours = create_time_string(@hour)
+    minutes = create_time_string(@minute)
+
+    "#{hours}:#{minutes}"
+  end
+
+  private
+
+  def create_time_string(number)
+    return number if number > 9
+    "0#{number}"
   end
 end
