f983b159c248704a18eb3acc983ba8f58a987b73
Andy Wendt <andy@awendt.com>
2019-09-25 20:56:56 -0600

    Implement the algo

--- ruby/luhn/luhn.rb
+++ ruby/luhn/luhn.rb
@@ -2,6 +2,16 @@ class Luhn
   def self.valid?(string)
     string = string.gsub(/[^0-9]/, '')
     return false unless string.length > 1
-    return true
+
+    numbers = string.reverse.split('').each_with_index.map do |number, index|
+      number = number.to_i
+      return number if (index % 2) != 0
+
+      number *= 2
+      number -= 9 if number > 10
+      number
+    end
+
+    (numbers.inject('+') % 10) == 0
   end
 end
