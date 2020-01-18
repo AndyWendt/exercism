d69dd438bfda0f937e892d6e0da6c550b7e7bdbf
Andy Wendt <awendt@mavenlink.com>
2020-01-17 21:12:53 -0700

    Complete basic tests

--- ruby/grains/grains.rb
+++ ruby/grains/grains.rb
@@ -2,6 +2,6 @@ class Grains
   def self.square(position)
     position_is_invalid = (position <= 0) || (position > 64)
     throw ArgumentError.new if position_is_invalid
-    1
+    (2).pow(position-1)
   end
 end
--- ruby/grains/grains_test.rb
+++ ruby/grains/grains_test.rb
@@ -8,32 +8,26 @@ class GrainsTest < Minitest::Test
   end
 
   def test_2
-    skip
     assert_equal 2, Grains.square(2)
   end
 
   def test_3
-    skip
     assert_equal 4, Grains.square(3)
   end
 
   def test_4
-    skip
     assert_equal 8, Grains.square(4)
   end
 
   def test_16
-    skip
     assert_equal 32_768, Grains.square(16)
   end
 
   def test_32
-    skip
     assert_equal 2_147_483_648, Grains.square(32)
   end
 
   def test_64
-    skip
     assert_equal 9_223_372_036_854_775_808, Grains.square(64)
   end
 
