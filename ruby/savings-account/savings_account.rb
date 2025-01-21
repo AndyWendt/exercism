4fc096339883620f240d5424983f51399193780b
Andy Wendt <andy@awendt.com>
2025-01-20 19:51:37 -0700

    Update savings account

--- ruby/savings-account/savings_account.rb
+++ ruby/savings-account/savings_account.rb
@@ -1,13 +1,25 @@
 module SavingsAccount
   def self.interest_rate(balance)
-    raise 'Please implement the SavingsAccount.interest_rate method'
+    return 0.5 if balance < 1000 && balance > 0
+    return 1.621 if balance >= 1000 && balance < 5000
+    return 2.475 if balance >= 5000
+    return 3.213 if balance < 0
+    0.5
   end
 
   def self.annual_balance_update(balance)
-    raise 'Please implement the SavingsAccount.annual_balance_update method'
+    return 0 if balance.eql?(0)
+    (1 + (self.interest_rate(balance) / 100)) * balance
   end
 
   def self.years_before_desired_balance(current_balance, desired_balance)
-    raise 'Please implement the SavingsAccount.years_before_desired_balance method'
+    years = 0
+
+    while current_balance < desired_balance
+      current_balance = self.annual_balance_update(current_balance)
+      years += 1
+    end
+
+    years
   end
 end
