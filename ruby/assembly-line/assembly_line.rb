071972f61bea731d0c36d7937f57a436b184618b
Andy Wendt <andy@awendt.com>
2025-01-20 19:22:53 -0700

    complete assembly line

--- ruby/assembly-line/assembly_line.rb
+++ ruby/assembly-line/assembly_line.rb
@@ -4,10 +4,20 @@ class AssemblyLine
   end
 
   def production_rate_per_hour
-    raise 'Please implement the AssemblyLine#production_rate_per_hour method'
+    return production_rate(0.9) if (5..8).include?(@speed)
+    return production_rate(0.8) if 9.eql?(@speed)
+    return production_rate(0.77) if 10.eql?(@speed)
+    production_rate
   end
 
   def working_items_per_minute
-    raise 'Please implement the AssemblyLine#working_items_per_minute method'
+    production_rate_per_hour.round / 60
+  end
+
+  private
+
+  def production_rate(success_rate = 1)
+    rate = @speed * 221
+    (rate * success_rate).round(3)
   end
 end
