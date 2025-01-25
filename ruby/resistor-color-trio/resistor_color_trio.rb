ef84e581a5f54d40f49b0f10fbc0c979939ef020
Andy Wendt <andy@awendt.com>
2025-01-24 19:13:11 -0700

    Add resistor color

---
+++ ruby/resistor-color-trio/resistor_color_trio.rb
@@ -0,0 +1,26 @@
+=begin
+Write your code for the 'Resistor Color Trio' exercise in this file. Make the tests in
+`resistor_color_trio_test.rb` pass.
+
+To get started with TDD, see the `README.md` file in your
+`ruby/resistor-color-trio` directory.
+=end
+
+class ResistorColorTrio
+  def initialize(trio)
+  end
+end
+
+
+=begin
+- black: 0
+- brown: 1
+- red: 2
+- orange: 3
+- yellow: 4
+- green: 5
+- blue: 6
+- violet: 7
+- grey: 8
+- white: 9
+=end
